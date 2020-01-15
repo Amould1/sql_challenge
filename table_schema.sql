1a4e8718e231788d1858da458e9e3d72e18b330c
Amould1 <55570454+Amould1@users.noreply.github.com>
2020-01-15 00:49:08 -0500

    Add files via upload

---
+++ table_schema.sql
@@ -0,0 +1,77 @@
+CREATE TABLE "departments" (
+    "dept_no" varchar   NOT NULL,
+    "dept_name" varchar   NOT NULL,
+    PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+Create table "dept_emp" (
+    "emp_no" int   NOT NULL,
+    "dept-no" varchar   NOT NULL,
+    "from_date" varchar   NOT NULL,
+    "to_date" varchar   NOT NULL,
+	PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" varchar   NOT NULL,
+    "emp_no" int   NOT NULL,
+    "from_date" varchar   NOT NULL,
+    "to_date" varchar   NOT NULL,
+    PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "employees" (
+    "emp_no" int   NOT NULL,
+    "birthdate" varchar   NOT NULL,
+    "first_name" varchar   NOT NULL,
+    "last_name" varchar   NOT NULL,
+    "gender" varchar   NOT NULL,
+    "hire_date" varchar   NOT NULL,
+    PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL,
+    "from_date" varchar   NOT NULL,
+    "to_date" varchar   NOT NULL,
+    PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "titles" (
+    "emp_no" int   NOT NULL,
+    "title" varchar   NOT NULL,
+    "from_date" varchar   NOT NULL,
+    "to_date" varchar   NOT NULL,
+    PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+ALTER TABLE "departments" ADD CONSTRAINT "fk_departments_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "dept_manager" ("dept_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept-no" FOREIGN KEY("dept-no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
